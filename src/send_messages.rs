95cf8d754d668143ca1c51cd9de7cfd5bf72e942
icsboyx <icsboyx@blkh.lan>
2023-08-05 20:44:25 +0200

    minor changes on comment lines.

--- src/send_messages.rs
+++ src/send_messages.rs
@@ -2,7 +2,7 @@ use std::io::Write;
 use std::net::TcpStream;
 
 pub fn send_messages(mut stream: &TcpStream, channel: &&str, message: String) {
-    // This function sends a sample message to all the specified channel.
+    // This function sends a sample message to the specified channel.
     let payload: String = format!("PRIVMSG #{} :{}\r\n", channel, message);
     stream
         .write_all(payload.as_bytes())
