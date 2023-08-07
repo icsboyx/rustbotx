ce8cc9be75fb1ed0bf17c2c6ba6ec52a97eb428f
icsboyx <icsboyx@blkh.lan>
2023-08-07 10:33:59 +0200

    console as trait

--- src/console.rs
+++ src/console.rs
@@ -1,11 +1,16 @@
+// console.rs
+
 use colored::*;
 
-pub fn start_console() {
-   //To Do
-}
+pub struct Console;
 
+impl Console {
+    pub fn console_start(&self) {
+        self.console_println("Console started");
+    }
 
-pub fn console_println<S: AsRef<str>>(message: S) {
-    let message_str: &str = message.as_ref();
-    println!("{}{}", "[CONSOLE]: ".blue().bold(), message_str);
+    pub fn console_println<S: AsRef<str>>(&self, message: S) {
+        let message_str: &str = message.as_ref();
+        println!("{} {}", "[CONSOLE]:".blue().bold(), message_str);
+    }
 }
--- src/receive_messages.rs
+++ src/receive_messages.rs
@@ -2,26 +2,26 @@
 use std::io::Read;
 use std::net::TcpStream;
 // Import the external custom modules
-use crate::console_println;
+use crate::Console;
 
 
 pub fn receive_messages(mut stream: TcpStream,) {
     let mut buffer = [0; 1024];
     loop {
-        console_println(format!("Waiting for data..."));
+        Console.console_println(format!("Waiting for data..."));
         match stream.read(&mut buffer) {
             Ok(bytes_read) => {
                 if bytes_read == 0 {
                     // End of stream, the server closed the connection
-                    console_println(format!("Connection closed by the server"));
+                    Console.console_println(format!("Connection closed by the server"));
                     break;
                 }
                 let message = std::str::from_utf8(&buffer[..bytes_read])
                     .expect("Failed to parse message");
-                console_println(format!("Received: {}", message));
+                Console.console_println(format!("Received: {}", message));
             }
             Err(e) => {
-                console_println(format!("Error reading from stream: {}", e));
+                Console.console_println(format!("Error reading from stream: {}", e));
                 break;
             }
         }
